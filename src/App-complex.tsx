7444f523551362412b54fe321c12d065365618c7
jairrodrigo <jairrodrigo13@gmail.com>
2025-07-28 06:51:15 -0300

    debug: versão super simples do App para testar deploy

---
+++ src/App-complex.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { MainLayout } from '@/components/layout'
+import { DashboardPage, NovaBobinaPage, NovoPedidoPage, PedidosPage } from '@/pages'
+import '@/index.css'
+
+type PageType = 'dashboard' | 'nova-bobina' | 'novo-pedido' | 'pedidos'
+
+function App() {
+  const [currentPage, setCurrentPage] = useState<PageType>('dashboard')
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'nova-bobina':
+        return <NovaBobinaPage />
+      case 'novo-pedido':
+        return <NovoPedidoPage />
+      case 'pedidos':
+        return <PedidosPage />
+      case 'dashboard':
+      default:
+        return <DashboardPage />
+    }
+  }
+
+  return (
+    <MainLayout currentPage={currentPage} onPageChange={setCurrentPage}>
+      {renderPage()}
+    </MainLayout>
+  )
+}
+
+export default App
